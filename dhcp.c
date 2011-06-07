59c19c0d0bc29c31f469ca4f8d1b4a1ea521113d
Andrew Lindsay <andrew@thiseldo.co.uk>
2011-06-07 19:40:41 +0100

    Massive rewrite of dhcp.c by JCW @ JeeLabs.org that should fix current issues

--- dhcp.c
+++ dhcp.c
@@ -3,6 +3,9 @@
  * To use the above modeline in vim you must have "set modeline" in your .vimrc
  *
  * Author: Andrew Lindsay
+ * Rewritten and optimized by Jean-Claude Wippler, http://jeelabs.org/
+ * Please visit Jeelabs for tons of cool stuff.
+ *
  * Copyright: GPL V2
  * See http://www.gnu.org/licenses/gpl.html
  *
@@ -15,6 +18,7 @@
 #include <stdlib.h>
 #include "ip_config.h"
 #include "net.h"
+#include "dhcp.h"
 #include "ip_arp_udp_tcp.h"
 
 #if defined (UDP_client) 
@@ -59,8 +63,6 @@ typedef struct dhcpData {
         // just tag onto end of structure
 } dhcpData;
 
-dhcpData *dhcpPtr;
-
 static uint8_t dhcptid_l=0; // a counter for transaction ID
 // src port high byte must be a a0 or higher:
 #define DHCPCLIENT_SRC_PORT_H 0xe0 
@@ -77,17 +79,18 @@ uint8_t *dhcpserver;
 uint8_t *dnsserver;
 uint8_t *gwaddr;
 
-static char hostname[16];
+static char hostname[] = "Arduino-00";
 static uint8_t haveDhcpAnswer=0;
 static uint8_t dhcp_ansError=0;
 uint32_t currentXid = 0;
 uint16_t currentSecs = 0;
 static uint32_t leaseStart = 0;
 static uint32_t leaseTime = 0;
+static uint8_t* bufPtr;
 
-// Functions prototypes
-uint8_t have_dhcpoffer(uint8_t *buf,uint16_t plen);
-uint8_t have_dhcpack(uint8_t *buf,uint16_t plen);
+static void addToBuf(uint8_t b) {
+    *bufPtr++ = b;
+}
 
 uint8_t dhcp_state(void)
 {
@@ -141,7 +144,9 @@ void dhcp_start(uint8_t *buf, uint8_t *macaddrin, uint8_t *ipaddrin,
           dnsserver[n] = 0;
         }
         // Set a unique hostname, use Arduino- plus last octet of mac address
-        sprintf( hostname, "Arduino-%02x", macaddr[6] );
+        // sprintf( hostname, "Arduino-%02x", macaddr[6] );
+        hostname[8] = 'A' + (macaddr[6] >> 4);
+        hostname[9] = 'A' + (macaddr[6] & 0x0F);
         dhcp_send( buf, DHCPDISCOVER );
         dhcpState = DHCP_STATE_DISCOVER;
 }
@@ -162,34 +167,21 @@ void dhcp_send(uint8_t *buf, uint8_t requestType ) {
         dhcp_ansError=0;
         dhcptid_l++; // increment for next request, finally wrap
         // destination IP gets replaced after this call
+       
+        memset(buf, 0, 400); //XXX OUCH!   
         send_udp_prepare(buf,(DHCPCLIENT_SRC_PORT_H<<8)|(dhcptid_l&0xff),dhcpip,DHCP_DEST_PORT);
 
-        while(i<6){
-                buf[ETH_DST_MAC +i]=0xff;
-                i++;
-        }
+        memset(buf + ETH_DST_MAC, 0xFF, 6);
         buf[IP_TOTLEN_L_P]=0x82;
         buf[IP_PROTO_P]=IP_PROTO_UDP_V;
-        i=0;
-        while(i<4){
-                buf[IP_SRC_P+i]=0;
-                buf[IP_DST_P+i]=0xff;
-                i++;
-        }
-        buf[UDP_DST_PORT_H_P]=0;
+        memset(buf + IP_DST_P, 0xFF, 4);
         buf[UDP_DST_PORT_L_P]=DHCP_SRC_PORT; 
         buf[UDP_SRC_PORT_H_P]=0;
         buf[UDP_SRC_PORT_L_P]=DHCP_DEST_PORT;
-        // Set length later when known
-        buf[UDP_LEN_H_P]=0;
-        buf[UDP_LEN_L_P]=0;
-        // zero the checksum
-        buf[UDP_CHECKSUM_H_P]=0;
-        buf[UDP_CHECKSUM_L_P]=0;
 
         // Build DHCP Packet from buf[UDP_DATA_P]
         // Make dhcpPtr start of UDP data buffer
-        dhcpPtr = &buf[UDP_DATA_P];
+        dhcpData *dhcpPtr = &buf[UDP_DATA_P];
         // 0-3 op, htype, hlen, hops
         dhcpPtr->op = DHCP_BOOTREQUEST;
         dhcpPtr->htype = 1;
@@ -197,136 +189,65 @@ void dhcp_send(uint8_t *buf, uint8_t requestType ) {
         dhcpPtr->hops = 0;
         // 4-7 xid
         dhcpPtr->xid = currentXid;
-
         // 8-9 secs
         dhcpPtr->secs = currentSecs;
-
-        // 10-11 flags
-        dhcpPtr->flags = 0x0000;
-        
-        // 12-15 ciaddr
-        for( i=0; i<4; i++)
-                dhcpPtr->ciaddr[i] = 0;
-
         // 16-19 yiaddr
-        for( i=0; i<4; i++)
-                dhcpPtr->yiaddr[i] = dhcpip[i];
-
-        // 20-23 siaddr
-        for( i=0; i<4; i++)
-                dhcpPtr->siaddr[i] = 0;
-
-        // 24-27 giaddr
-        for( i=0; i<4; i++)
-                dhcpPtr->giaddr[i] = 0;
-
+        memset(dhcpPtr->yiaddr, 0, 4);
         // 28-43 chaddr(16)
-        for( i=0; i<6; i++)
-                dhcpPtr->chaddr[i] = macaddr[i];
-        for( i=6; i<16; i++)
-                dhcpPtr->chaddr[i] = 0;
+        memcpy(dhcpPtr->chaddr, macaddr, 6);
 
-        // 44-107 sname (64)
-        for( i=0; i<64; i++)
-                dhcpPtr->sname[i] = 0;
-        // 108-235 file(128)
-        for( i=0; i<128; i++)
-                dhcpPtr->file[i] = 0;
         // options defined as option, length, value
-        int optionIndex = sizeof( dhcpData );
+        bufPtr = buf + UDP_DATA_P + sizeof( dhcpData );
         // Magic cookie 99, 130, 83 and 99
-        buf[UDP_DATA_P + optionIndex] = 99;
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 130;
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 83;
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 99;
-        optionIndex++;
+        addToBuf(99);
+        addToBuf(130);
+        addToBuf(83);
+        addToBuf(99);
         
         // Set correct options
         // Option 1 - DHCP message type
-        buf[UDP_DATA_P + optionIndex] =  53;   // DHCPDISCOVER, DHCPREQUEST
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 1;      // Length 
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = requestType;      // Value
-        optionIndex++;
+        addToBuf(53);   // DHCPDISCOVER, DHCPREQUEST
+        addToBuf(1);      // Length 
+        addToBuf(requestType);      // Value
 
-        // Define options based on if we are reply or request
-/*
-        if( requestType == DHCPDISCOVER ) {  
-                // Option 2
-                buf[UDP_DATA_P + optionIndex] = 116;     // Auto configuration
-                optionIndex++;
-                buf[UDP_DATA_P + optionIndex] = 1;      // Length 
-                optionIndex++;
-                buf[UDP_DATA_P + optionIndex] = 0x01;      // Value
-                optionIndex++;
-        }
-*/
         // Client Identifier Option, this is the client mac address
-        buf[UDP_DATA_P + optionIndex] = 61;     // Client identifier
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 7;      // Length 
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 0x01;      // Value
-        optionIndex++;
+        addToBuf(61);     // Client identifier
+        addToBuf(7);      // Length 
+        addToBuf(0x01);      // Value
         for( i=0; i<6; i++)
-                buf[UDP_DATA_P + optionIndex +i] = macaddr[+i];
-        optionIndex += 6;
+                addToBuf(macaddr[i]);
 
-        // Host name Option, we set it as Arduino-xx where xx is the last 2 digits of the mac address 
-        buf[UDP_DATA_P + optionIndex] = 12;     // Host name
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 10;      // Length 
-        optionIndex++;
-        // setup hostname
-        i = 0;
-        while( hostname[i] != 0 ) {
-                buf[UDP_DATA_P + optionIndex +i] = hostname[i];
-                i++;
-        }
- //       buf[UDP_DATA_P + optionIndex + i] = 0;
- //       i++;
-        optionIndex += i;
+        // Host name Option
+        addToBuf(12);     // Host name
+        addToBuf(10);      // Length 
+        for( i=0; i<10; i++)
+                addToBuf(hostname[i]);
 
         if( requestType == DHCPREQUEST ) {
                 // Request IP address
-                buf[UDP_DATA_P + optionIndex] = 50;     // Requested IP address
-                optionIndex++;
-                buf[UDP_DATA_P + optionIndex] = 4;      // Length 
-                optionIndex++;
+                addToBuf(50);     // Requested IP address
+                addToBuf(4);      // Length 
                 for( i=0; i<4; i++)
-                        buf[UDP_DATA_P + optionIndex +i] = dhcpip[i];
-                optionIndex += 4;
+                        addToBuf(dhcpip[i]);
 
                 // Request using server ip address
-                buf[UDP_DATA_P + optionIndex] = 54;     // Server IP address
-                optionIndex++;
-                buf[UDP_DATA_P + optionIndex] = 4;      // Length 
-                optionIndex++;
+                addToBuf(54);     // Server IP address
+                addToBuf(4);      // Length 
                 for( i=0; i<4; i++)
-                        buf[UDP_DATA_P + optionIndex +i] = dhcpserver[i];
-                optionIndex += 4;
+                        addToBuf(dhcpserver[i]);
+
         }
 
         // Additional information in parameter list - minimal list for what we need
-        buf[UDP_DATA_P + optionIndex] = 55;     // Parameter request list
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 3;      // Length 
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 1;      // Subnet mask
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 3;      // Route/Gateway
-        optionIndex++;
-        buf[UDP_DATA_P + optionIndex] = 6;      // DNS Server
-        optionIndex++;
-//        buf[UDP_DATA_P + optionIndex] = 15;     // Domain name
-//        optionIndex++;
+        addToBuf(55);     // Parameter request list
+        addToBuf(3);      // Length 
+        addToBuf(1);      // Subnet mask
+        addToBuf(3);      // Route/Gateway
+        addToBuf(6);      // DNS Server
 
         // payload len should be around 300
-        send_udp_transmit(buf, optionIndex);
+        addToBuf(255);      // end option
+        send_udp_transmit(buf, bufPtr - buf - UDP_DATA_P);
 }
 
 // Examine packet, if dhcp then process, if just exit.
@@ -337,121 +258,59 @@ void dhcp_send(uint8_t *buf, uint8_t requestType ) {
 // Either DHCPOFFER, DHCPACK or DHCPNACK
 // Return 0 for nothing processed, 1 for done soemthing
 uint8_t check_for_dhcp_answer(uint8_t *buf, uint16_t plen){
-        uint8_t j,i;
-
+    // Map struct onto payload
+    dhcpData *dhcpPtr = &buf[UDP_DATA_P];
+    if (plen >= 70 && buf[UDP_SRC_PORT_L_P] == DHCP_SRC_PORT &&
+            dhcpPtr->op == DHCP_BOOTREPLY && dhcpPtr->xid == currentXid ) {
         // Check for lease expiry
-        uint32_t currentSecs = millis();
-        if (plen<70){
-                return(0);
-        }
-        if (buf[UDP_SRC_PORT_L_P]!=DHCP_SRC_PORT){
-                // not from a DHCP
-                return(0);
-        }
-
-        // Map struct onto payload
-        dhcpPtr = &buf[UDP_DATA_P];
-
-        // Must be a reply
-        if( dhcpPtr->op != DHCP_BOOTREPLY ) {
-                // Not a reply
-                return(0);
-        }
-
-        // Must be for our transaction ID
-        if( dhcpPtr->xid != currentXid ) {
-                // Not a response to us
-                return(0);
-        }
-
-        // Get Type, either Offer, Ack or Nack
-        // Find Options - at end of struct + 4 to skip magic cookie
-        //
+        // uint32_t currentSecs = millis();
         int optionIndex = UDP_DATA_P + sizeof( dhcpData ) + 4;
-        uint8_t dhcpType = 0;
-        if( buf[optionIndex] == 53 ) {
-                // Type option
-                //optionIndex++;
-                dhcpType = buf[optionIndex+2];
-                switch( dhcpType ) {
-                        case DHCPOFFER:
-                                return(have_dhcpoffer( buf, plen ));
-                        case DHCPACK:
-                                return(have_dhcpack( buf, plen ));
-                }
+        if( buf[optionIndex] == 53 )
+            switch( buf[optionIndex+2] ) {
+                case DHCPOFFER: return have_dhcpoffer( buf, plen );
+                case DHCPACK:   return have_dhcpack( buf, plen );
+            }
+    }
+    return 0;
+}
+
+
+uint8_t have_dhcpoffer (uint8_t *buf,uint16_t plen) {
+    // Map struct onto payload
+    dhcpData *dhcpPtr = buf + UDP_DATA_P;
+    // Offered IP address is in yiaddr
+    memcpy(dhcpip, dhcpPtr->yiaddr, 4);
+    // Scan through variable length option list identifying options we want
+    uint8_t *ptr = (uint8_t*) (dhcpPtr + 1) + 4;
+    do {
+        uint8_t option = *ptr++;
+        uint8_t optionLen = *ptr++;
+        uint8_t i;
+        switch (option) {
+            case 1:  memcpy(dhcpmask, ptr, 4);
+                     break;
+            case 3:  memcpy(gwaddr, ptr, 4);
+                     break;
+            case 6:  memcpy(dnsserver, ptr, 4);
+                     break;
+            case 51: leaseTime = 0;
+                     for (i = 0; i<4; i++)
+                         leaseTime = (leaseTime + ptr[i]) << 8;
+                     leaseTime *= 1000;      // milliseconds
+                     break;
+            case 54: memcpy(dhcpserver, ptr, 4);
+                     break;
         }
-
-        return( 0 );
+        ptr += optionLen;
+    } while (ptr < buf + plen);
+    dhcp_request_ip( buf );
+    return 1;
 }
 
-
-uint8_t have_dhcpoffer(uint8_t *buf,uint16_t plen) {
-  // Map struct onto payload
-  dhcpPtr = &buf[UDP_DATA_P];
-  int optionIndex = UDP_DATA_P + sizeof( dhcpData ) + 4;
-
-  // Offered IP address is in yiaddr
-  uint8_t n;
-  for( n=0; n<4; n++ )
-          dhcpip[n] = dhcpPtr->yiaddr[n];
-
-  // Scan through variable length option list identifying 
-  // options we want
-  uint8_t moreOptions = 1;
-  int optionLen = 0;
-  int i = 0;
-  while( moreOptions > 0 ) {
-          optionLen = buf[optionIndex+1];
-          switch( buf[optionIndex] ) {
-                // Option: (t=1,l=4) Subnet Mask = 255.255.255.0
-                case 1:
-                        for( i = 0; i<4; i++ )
-                                dhcpmask[i] = buf[optionIndex+2+i];
-                        break;
-                // Option: (t=3,l=4) Router = 192.168.1.1
-                case 3:
-                        for( i = 0; i<4; i++ )
-                                gwaddr[i] = buf[optionIndex+2+i];
-                        break;
-                // Option: (t=6,l=8) Domain Name Server
-                case 6:
-                        for( i = 0; i<4; i++ )
-                                dnsserver[i] = buf[optionIndex+2+i];
-                        break;
-                // Option: (t=15,l=4) Domain Name = "home"
-                case 15:
-                        break;
-                // Option: (t=51,l=4) IP Address Lease Time = 1 day
-                case 51:
-                        leaseTime = 0;
-                        for( i = 0; i<4; i++ ) {
-                                leaseTime += buf[optionIndex+2+i];
-                                leaseTime = leaseTime << 8;
-                        }
-                        leaseTime *= 1000;      // milliseconds
-                        break;
-                // Option: (t=54,l=4) Server Identifier = 192.168.1.1
-                case 54:
-                        for( i = 0; i<4; i++ )
-                                dhcpserver[i] = buf[optionIndex+2+i];
-                        break;
-                default:
-                        break;
-          }
-          // Just move index
-          optionIndex += buf[optionIndex+1] +2;
-          if( optionIndex >= plen ) 
-                  moreOptions = 0;
-  }
-  dhcp_request_ip( buf );
-  return(1);
-}
-
-uint8_t have_dhcpack(uint8_t *buf,uint16_t plen) {
-  dhcpState = DHCP_STATE_OK;    // Complete for now
-  leaseStart = millis();
-
-  return(2);
+uint8_t have_dhcpack (uint8_t *buf,uint16_t plen) {
+    dhcpState = DHCP_STATE_OK;
+    leaseStart = millis();
+    return 2;
 }
 
 #endif
